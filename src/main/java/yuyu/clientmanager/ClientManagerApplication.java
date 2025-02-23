0a372530b4a54ef58eacdce43645a831a288e2b6
JelinkovaTera <jelinkova.tera@gmail.com>
2025-02-23 15:37:09 +0100

    gui instance in Application

--- src/main/java/yuyu/clientmanager/ClientManagerApplication.java
+++ src/main/java/yuyu/clientmanager/ClientManagerApplication.java
@@ -1,27 +1,18 @@
 package yuyu.clientmanager;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import yuyu.clientmanager.gui.ClientManagerGUI;
 
 import javax.swing.*;
 
 @SpringBootApplication
 public class ClientManagerApplication {
-    private final ClientManagerGUI gui;
-
-    ClientManagerApplication(ClientManagerGUI gui) {
-        this.gui = gui;
-    }
 
     public static void main(String[] args) {
         System.setProperty("java.awt.headless", "false");
-        SpringApplication.run(ClientManagerApplication.class, args);
-    }
-
-    @PostConstruct
-    public void init() {
-        SwingUtilities.invokeLater(gui::launchGUI);
+        ConfigurableApplicationContext context = SpringApplication.run(ClientManagerApplication.class, args);
+        SwingUtilities.invokeLater(() -> context.getBean(ClientManagerGUI.class).launchGUI());
     }
 }
